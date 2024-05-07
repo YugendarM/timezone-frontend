8ccd08fc62a1f0e2ec54bf469d21b88e75c5dd9c
YugendarM <yugendar2411@gmail.com>
2024-05-07 18:40:13 +0530

    Initial commit

---
+++ src/Components/HomePageComponent/HomePageComponent.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const HomePageComponent = () => {
+  return (
+    <div>
+      <p className='text-3xl bg-blue-400'>HomePageComponent</p>
+    </div>
+  )
+}
+
+export default HomePageComponent
